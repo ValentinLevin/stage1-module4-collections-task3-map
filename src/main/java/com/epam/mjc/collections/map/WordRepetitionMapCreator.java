f8f5ec0a61639070615974a403cf95aea6b91c01
Valentin Levin <levin_valentin@mail.ru>
2023-12-22 14:36:00 +0600

    solution #1

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,19 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> result = new HashMap<>();
+        String[] words = sentence.split("\\W+");
+        for (String word: words) {
+            if (!word.isEmpty()) {
+                String wordInLowerCase = word.toLowerCase();
+                Integer count = result.getOrDefault(wordInLowerCase, 0);
+                result.put(wordInLowerCase, count + 1);
+            }
+        }
+        return result;
     }
 }
